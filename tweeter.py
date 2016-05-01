21788ec345aeb01a13b4cd2cde89ab948ab97ca6
corbinq27 <corbinjohnson@me.com>
2016-05-01 13:29:49 -0500

    uses /tmp folder to retrieve and save files to.
    Also uses new S3 file uploader and file downloader to retrieve and store necessary files in s3.  it's a bit of hardcoded nonsense not necessary to run without s3.

--- tweeter.py
+++ tweeter.py
@@ -5,48 +5,67 @@ import json
 from main import CreateData
 from product_extractor import ProductExtractor
 from pyshorteners import Shortener
+import time
+import requests
+from s3_file_download import FileDownload
+from s3_file_upload import FileUpload
 
 tweeter_keys = {}
 
-with open("twitter_keys.json", "rb") as fp:
-    tweeter_keys = json.load(fp)
-
-twitter = Twython(tweeter_keys["APP_KEY"], tweeter_keys["APP_SECRET"], tweeter_keys["OAUTH_TOKEN"],
-                    tweeter_keys["OAUTH_TOKEN_SECRET"])
-
-pe = ProductExtractor()
-pe.product_extractor()
-
-cd = CreateData()
-cd.create_data()
-
-price_comparison = {}
-
-with open("price-comparison-recent.json", "rb") as fp:
-    price_comparison = json.load(fp)
-
-#if any price comparisons have changed (as noted in their data) then we'll tweet the product and how it changed.
-
-if price_comparison:
-    for product, info in price_comparison.iteritems():
-        if info["is_discontinued_product"]:
-            shortener = Shortener('Google', api_key = tweeter_keys["GOOGLE_URL_SHORTENER_KEY"])
-            short_url = shortener.short(info["product_url"])
-            twitter.update_status(status="%s no longer for sale. %s" % (product, short_url))
-        elif info["is_new_product"]:
-            shortener = Shortener('Google', api_key = tweeter_keys["GOOGLE_URL_SHORTENER_KEY"])
-            short_url = shortener.short(info["product_url"])
-            twitter.update_status(status="%s now available! $%s. %s" % (product, int(info["new_price"]), short_url))
-        elif info["is_difference"]:
-            shortener = Shortener('Google', api_key = tweeter_keys["GOOGLE_URL_SHORTENER_KEY"])
-            short_url = shortener.short(info["product_url"])
-            twitter.update_status(status='New price for %s: $%s. Was $%s. %s' % (product, int(info["new_price"]),
-                                                                                 int(info["old_price"]), short_url))
-        else:
-            print "no change for %s" % product
-            shortener = Shortener('Google', api_key = tweeter_keys["GOOGLE_URL_SHORTENER_KEY"])
-            short_url = shortener.short(info["product_url"])
-            print 'test printout for %s: $%s. Was $%s. %s' % (product, int(info["new_price"]),
-                                                                                 int(info["old_price"]), short_url)
+def lambda_handler(event, context):
 
 
+    with open("twitter_keys.json", "rb") as fp:
+        tweeter_keys = json.load(fp)
+
+    twitter = Twython(tweeter_keys["APP_KEY"], tweeter_keys["APP_SECRET"], tweeter_keys["OAUTH_TOKEN"],
+                        tweeter_keys["OAUTH_TOKEN_SECRET"])
+
+    fdownload = FileDownload()
+    fdownload.file_download()
+
+    pe = ProductExtractor()
+    pe.product_extractor()
+
+    cd = CreateData()
+    new_data_filename = cd.create_data()
+
+
+
+    fupload = FileUpload()
+    if new_data_filename:
+        fupload.file_upload([new_data_filename])
+    else:
+        fupload.file_upload()
+
+    price_comparison = {}
+
+    with open("/tmp/price-comparison-recent.json", "rb") as fp:
+        price_comparison = json.load(fp)
+
+    #if any price comparisons have changed (as noted in their data) then we'll tweet the product and how it changed.
+
+    if price_comparison:
+        for product, info in price_comparison.iteritems():
+            short_url_obtained = False
+
+            try:
+                shortener = Shortener('Google', api_key = tweeter_keys["GOOGLE_URL_SHORTENER_KEY"])
+                short_url = shortener.short(info["product_url"])
+                short_url_obtained = True
+            except(requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+                time.sleep(2)
+                if not short_url_obtained:
+                    short_url = "http://goo.gl/p7KqbO" #hardcoded to wholesalegaming.biz
+
+            if info["is_discontinued_product"]:
+                twitter.update_status(status="%s no longer for sale. %s" % (product, short_url))
+            elif info["is_new_product"]:
+                twitter.update_status(status="%s now available! $%s. %s" % (product, int(info["new_price"]), short_url))
+            elif info["is_difference"]:
+                twitter.update_status(status='New price for %s: $%s. Was $%s. %s' % (product, int(info["new_price"]),
+                                                                                     int(info["old_price"]), short_url))
+            else:
+                print "no change for %s" % product
+                print 'test printout for %s: $%s. Was $%s. %s' % (product, int(info["new_price"]),
+                                                                                     int(info["old_price"]), short_url)
